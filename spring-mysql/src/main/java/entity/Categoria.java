f26e182024a57c8d11fcbb8fc5f87d6b34abda11
Gasipe <gblsilvap@gmail.com>
2026-01-22 11:12:29 -0300

    criacao da entidade categoria

---
+++ spring-mysql/src/main/java/entity/Categoria.java
@@ -0,0 +1,39 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String nome;
+
+    @Column(length = 500)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal precoDiario;
+
+    @Column(nullable = false)
+    private Integer quantidadeDePortas;
+
+    @Column(nullable = false)
+    private Integer limitePassageiros;
+
+
+
+}
---
+++ spring-mysql/src/main/java/entity/Veiculo.java
@@ -0,0 +1,6 @@
+package entity;
+
+import jakarta.persistence.Entity;
+
+public class Veiculo {
+}
